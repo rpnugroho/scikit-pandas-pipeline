3413a098724f7336015d8c55d1ebcdc2fa5cbfb2
rpnugroho <nugroho.rizkyp@gmail.com>
2021-03-13 16:39:29 +0700

    add DFScaler class

--- skpdspipe/scale.py
+++ skpdspipe/scale.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import (StandardScaler, MinMaxScaler,
+                                   MaxAbsScaler, RobustScaler, Normalizer)
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -10,6 +11,36 @@ class DFStandardScaler(BaseEstimator, TransformerMixin):
 
         """
         self.scaler = StandardScaler()
+
+    def fit(self, X, y=None):
+        self.scaler.fit(X)
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        return pd.DataFrame(self.scaler.transform(X),
+                            columns=X.columns,
+                            index=X.index)
+
+
+class DFScaler(BaseEstimator, TransformerMixin):
+
+    def __init__(self, scaler_type='standard'):
+        """Scikit-learn scaler wrapper to return pandas DataFrame.
+
+        Args:
+            scaler_type (str, optional):
+                Scaler type 'standard', 'minmax', 'maxabs', 'robust', and 'normalizer'.
+                Defaults to 'standard'.
+        """
+        scalers = {
+            'standard': StandardScaler(),
+            'minmax': MinMaxScaler(),
+            'maxabs': MaxAbsScaler(),
+            'robust': RobustScaler(),
+            'normalizer': Normalizer()
+        }
+        self.scaler = scalers[scaler_type]
 
     def fit(self, X, y=None):
         self.scaler.fit(X)
